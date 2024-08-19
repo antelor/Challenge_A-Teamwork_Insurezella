95fc51bf0e6dae75308eb8de3b6308d9f70bc7a2
antelor <ramiro.antelo4@gmail.com>
2024-08-19 12:40:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='site-content'>
+      <nav>
+        <span>logo</span>
+        <span>Want to connect with us?</span>
+        <span>Mail us: support@insurezella.com</span>
+        <span>Call us: (877) 227 0774</span>
+      </nav>
+      
+      <div>test</div>
+    </div>
+  )
+}
+
+export default App
